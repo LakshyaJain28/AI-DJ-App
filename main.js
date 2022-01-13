91834f2d45bdb728aa139c9973f2ce6e6970b4b6
LakshyaJain28 <79844523+LakshyaJain28@users.noreply.github.com>
2022-01-13 15:57:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-song="";
+song1="";
+song2="";
+
+song1_status="";
+song2_status="";
 
 function preload()
 {
-    song=loadSound("music.mp3");
+    song1=loadSound("music.mp3");
+    song2=loadSound("faded.mp3")
 }
 scoreRightWrist=0;
 scoreLeftWrist=0;
@@ -19,6 +24,8 @@ function setup()
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses)
 }
 function modelLoaded()
 {
@@ -35,15 +42,43 @@ function gotPoses(results)
 
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
+
+        scoreRightWrist=results[0].pose.keypoints[10].score;
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
     }
 }
 function draw()
 {
     image(video,0,0,600,500);
+    song1_staus=song1.isPlaying();
+    song2_staus=song2.isPlaying();
+    fill("red");
+    stroke("red");
+    if(scoreRightWrist>0.2)
+    {
+        circle(rightWristX,rightWristY,20);
+        song2.stop();
+        if(song1_staus==false){
+            song1.play();
+            document.getElementById("song_name").innerHTML="Playing Harry Potter Theme Song.";
+
+        }
+    }
+    if(scoreLeftWrist>0.2)
+    {
+        circle(leftWristX,leftWristY,20);
+        song1.stop();
+        if(song2_staus==false){
+            song1.play();
+            document.getElementById("song_name").innerHTML="Playing The Faded Song.";
+            
+        }
+    }
+
 }
 function play()
 {
-    song.play();
-    song.setVolume(1);
-    song.rate(1);
+    song1.play();
+    song1.setVolume(1);
+    song1.rate(1);
 }
